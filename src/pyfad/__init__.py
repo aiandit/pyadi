b2407d4214f43feecf482b81e35d9c58dcdd4e0f
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-06-25 12:28:44 +0200

    Export more

--- src/pyfad/__init__.py
+++ src/pyfad/__init__.py
@@ -1,5 +1,7 @@
 
 version = "1.0.0"
 
-from .pyfad import D, DiffFor, Diff, Dpy, diff2pys, differentiate
+from .pyfad import DiffFor, DiffFD
+from .pyfad import D, Diff, Dpy, diff2pys, differentiate, dargs, dzeros
+from .pyfad import nvars, varv, fill
 from .astvisitor import locals, py, canonicalize, normalize
