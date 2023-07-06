ccf28e02cc80d5b195752aef73b47722c19046b3
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-06 23:39:01 +0200

    Can have two types of calls

--- src/pyfad/dummyad.py
+++ src/pyfad/dummyad.py
@@ -1,22 +1,46 @@
+import sys
 from .astvisitor import isbuiltin
 from .runtime import unjnd, dzeros
+
+
+me = sys.modules[__name__]
+
+
+call2 = ['super']
+
 
 def mkCall(f):
     def run(*args, **kw):
         d_kw, kw = unjnd(kw)
-        # print(f'Run function {f.__name__} ({args}), kw={kw}, d_kw={d_kw}')
-        # dres = f(*args[0::2], **d_kw)
+        print(f'Run function {f.__name__} ({args}), kw={kw}, d_kw={d_kw}')
+        dres = f(*args[1::2], **kw)
+        dres = dzeros(dres)
         res = f(*args[1::2], **kw)
-        dres = dzeros(res)
-        #print(f'dzeros {res} {dres}')
+        print(f'dzeros {res} {dres}')
         return dres, res
     return run
+
+
+def mkCall2(f):
+    def run2(*args, **kw):
+        d_kw, kw = unjnd(kw)
+        # print(f'Run function {f.__name__} ({args}), kw={kw}, d_kw={d_kw}')
+        dres = f(*args[0::2], **d_kw)
+        res = f(*args[1::2], **kw)
+        #dres = dzeros(res)
+        #print(f'dzeros {res} {dres}')
+        return dres, res
+    return run2
+
 
 def decorator(**opts):
 
     def inner(done, key, f, *args, **kw):
 
         print(f'D1 {f.__name__} before')
+
+        if f.__name__ in call2:
+            return mkCall2(f)
 
         if isbuiltin(f):
             return mkCall(f)
