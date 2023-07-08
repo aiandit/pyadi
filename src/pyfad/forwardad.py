b6501b606689134f68d0f0192f38f7aa7c9c3c8a
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-09 01:33:51 +0200

    rid() redefined => rename all the builtins rules

--- src/pyfad/forwardad.py
+++ src/pyfad/forwardad.py
@@ -1,17 +1,13 @@
 from itertools import chain
 from math import sin, cos, tan, asin, acos, atan, log, sqrt
-from .astvisitor import getmodule
+from .astvisitor import getmodule, rid
 from .runtime import unjnd
 import sys
+import numpy as np
 
 
 me = sys.modules[__name__]
 
-
-def rid(func):
-    mod, _ = getmodule(func)
-    fid = f'{func.__qualname__}_{mod}'.replace('.', '_')
-    return fid
 
 def mkRule(f, rule):
     def runRule(*args, **kw):
@@ -59,87 +55,101 @@ dict = {}
 hidden = {}
 
 
-def D_print_builtins(r, *args):
+def D_builtins_print(r, *args):
     print('D ', *args[0::2])
     return 0
 
-def D_super_builtins(r, *args):
+def D_builtins_super(r, *args):
     print('D_super', *args)
     return super(*args[0::2])
 
-def E_super___init___builtins(*args, **kw):
+def E_builtins_super___init__(*args, **kw):
     print('E_super___init___builtins', *args)
     return args[0], args[1]
 
-def E_object___init___builtins(*args, **kw):
+def E_builtins_object___init__(*args, **kw):
     print('E_object___init___builtins', *args)
     return args[0], args[1]
 
-def D_dict_items_builtins(r, dx, x):
+def D_builtins_dict_items(r, dx, x):
     return dx.items()
 
-def D_dict_keys_builtins(r, dx, x):
+def D_builtins_dict_keys(r, dx, x):
     return r
 
-def D_dict_values_builtins(r, dx, x):
+def D_builtins_dict_values(r, dx, x):
     return dx.values()
 
-def D_range_builtins(r, dx, x):
+def D_builtins_range(r, *args):
     return [0]*len(r)
 
-def D_len_builtins(r, dx, x):
+def D_builtins_len(r, dx, x):
     return 0
 
-def D_enumerate_builtins(r, dx, x):
+def D_builtins_enumerate(r, dx, x):
     return zip([0]*len(x), dx)
 
-def D_list_builtins(r, dx, x):
+def D_builtins_list(r, dx, x):
     return list(dx)
 
-def D_int_builtins(r, dx, x): return 0
-def D_float_builtins(r, dx, x): return float(dx)
-def D_complex_builtins(r, dx, x, dy, y): return complex(dx, dy)
+def D_builtins_int(r, dx, x): return 0
+def D_builtins_float(r, dx, x): return float(dx)
+def D_builtins_complex(r, dx, x, dy, y): return complex(dx, dy)
 
-def D_Random_random_builtins(r):
+def D_builtins_Random_random(r):
     return 0
 
-def D_min_builtins(r, dx, x, dy, y):
+def D_builtins_min(r, dx, x, dy, y):
     return dx if x < y else dy
 
-def D_max_builtins(r, dx, x, dy, y):
+def D_builtins_max(r, dx, x, dy, y):
     return dx if not(x < y) else dy
 
 
-def D_abs_builtins(r, dx, x):
+def D_builtins_abs(r, dx, x):
     return 1 if not(x < 0) else -1
 
 
-def D_sum_builtins(r, dx, x):
+def D_builtins_sum(r, dx, x):
     return sum(dx)
 
 
-def D_sin_math(r, dx, x):
+def D_math_sin(r, dx, x):
     return dx * cos(x)
 
-def D_cos_math(r, dx, x):
+def D_math_cos(r, dx, x):
     return dx * -sin(x)
 
-def D_tan_math(r, dx, x):
+def D_math_tan(r, dx, x):
     return dx / cos(x)**2
 
 
-def D_asin_math(r, dx, x):
+def D_math_asin(r, dx, x):
     return dx / sqrt(1 - x**2)
 
-def D_acos_math(r, dx, x):
+def D_math_acos(r, dx, x):
     return -dx / sqrt(1 - x**2)
 
-def D_atan_math(r, dx, x):
+def D_math_atan(r, dx, x):
     return dx / (1 + x**2)
 
 
-def D_log_math(r, dx, x):
+def D_math_log(r, dx, x):
     return dx / x
 
-def D_sqrt_math(r, dx, x):
+def D_math_sqrt(r, dx, x):
     return 0.5 * dx / r
+
+
+def D_numpy_zeros(r, dshape, shape):
+    return r.copy()
+
+def D_numpy_savetxt(r, *args):
+    return r
+
+def D_numpy_matmul(r, dx, x, dy, y):
+    return np.matmul(dx, y) + np.matmul(x, dy)
+
+def E_builtins_ndarray_copy(*args, **kw):
+    print('builtins_ndarray_copy', args)
+    return args[0].copy(),  args[1].copy()
