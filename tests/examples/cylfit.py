3386f6e6062b19527a1a935dabb3ba10d6e3379a
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-16 15:45:20 +0200

    Cylfit optim with usolve and FD/AD working

--- tests/examples/cylfit.py
+++ tests/examples/cylfit.py
@@ -152,6 +152,78 @@ def runopt_ad():
     return runopt(grad)
 
 
+
+
+def runfsolve(fprime=None):
+    import scipy as sc
+
+    objComps, obj, handle = cylfit_obj()
+
+    R0 = 1.1
+    theta0 = 0.1
+    phi0 = np.pi/2
+
+    # v0 = np.array([1, 0, 0])
+    v0 = np.array([1, 0.01, 0.01])
+
+    N = int(1e3)**2
+
+    demopts = mkCylData(N, R0, theta0, phi0)
+
+    handle()['points'] = demopts
+
+    r0 = objComps(v0)
+
+    vs = v0.copy()
+    rs = r0.copy()
+
+    print('start usolve')
+    res = sc.optimize.fsolve(objComps, v0, fprime=fprime)
+    print('usolve result', res, vs)
+
+    sol = vs
+
+    sol[1] *= -1
+    sol[2] *= -1
+
+    sol[1] %= np.pi
+    sol[2] %= np.pi
+
+    rsol = obj(sol)
+
+    errsol = relNormMax(sol, np.array([R0, theta0, phi0]))
+    print(f'usolve solution {sol}, expected {[R0, theta0, phi0]}')
+    print(f'usolve solution error {errsol}, final objective {rsol}')
+
+
+def runfsolve_fd(fprime=None):
+    objComps, obj, handle = cylfit_obj()
+
+    def gobj(x, udata):
+        (dr, r) = pyfad.DiffFDNP(objComps, x)
+        g = np.zeros(r.size, x.size)
+        for i in range(x.size):
+            g[:,i] = dr[i]
+        print(f'gobj(x) = {np.linalg.norm(r)}')
+        return g
+
+    runfsolve(gobj)
+
+
+def runfsolve_ad(fprime=None):
+    objComps, obj, handle = cylfit_obj()
+
+    def gobj(x, udata):
+        (dr, r) = pyfad.DiffFor(objComps, x)
+        g = np.zeros(r.size, x.size)
+        for i in range(x.size):
+            g[:,i] = dr[i]
+        print(f'gobj(x) = {np.linalg.norm(r)}')
+        return g
+
+    runfsolve(gobj)
+
+
 def runuopt(fprime=None):
     import uopt.uopt
     import scipy as sc
@@ -192,8 +264,7 @@ def runuopt(fprime=None):
     res = uopt.uopt.uopt(v0, vs, rs, fobj_uopt, gobj_uopt)
     print(res)
 
-    sol, *rem = res
-
+    sol = vs
     rsol = obj(sol)
 
     errsol = relNormMax(sol, np.array([R0, theta0, phi0]))
@@ -201,7 +272,7 @@ def runuopt(fprime=None):
     print(f'CG solution error {errsol}, final objective {rsol}')
 
 
-def runusolve(fprime=None):
+def _runusolve(fprime=None, fvprime=None):
     import uopt.uopt
     import scipy as sc
 
@@ -212,20 +283,10 @@ def runusolve(fprime=None):
         print(f'obj(x) = {np.linalg.norm(y)}')
 
     def gobj(x, y, g, udata):
-        (dr, r) = pyfad.DiffFor(objComps, x)
-        y[:] = r
-        for i in range(x.size):
-            g[:,i] = dr[i]
-        print(f'gobj(x) = {np.linalg.norm(y)}')
+        fprime(objComps, x, y, g, udata)
 
     def gvobj(x, y, dx, g, udata):
-        (dr, r) = pyfad.DiffFor(objComps, x, seed=[dx])
-        y[:] = r
-        N, Ndd = dx.shape
-        for i in range(Ndd):
-            g[:,i] = dr[i]
-        print(f'gvobj(x) = {np.linalg.norm(y)}')
-
+        fvprime(objComps, x, y, dx, g, udata)
 
     R0 = 1.1
     theta0 = 0.1
@@ -238,6 +299,8 @@ def runusolve(fprime=None):
 
     demopts = mkCylData(N, R0, theta0, phi0)
 
+    #demopts += (np.random.rand(N, 3) - 0.5) * 1e-6
+
     handle()['points'] = demopts
 
     r0 = objComps(v0)
@@ -246,7 +309,10 @@ def runusolve(fprime=None):
     rs = r0.copy()
 
     print('start usolve')
-    res = uopt.uopt.usolve(v0, vs, rs, fobj, gobj, gvobj)
+    res = uopt.uopt.usolve(v0, vs, rs,
+                           fobj,
+                           gobj if fprime is not None else None,
+                           gvobj if fvprime is not None else None)
     print('usolve result', res, vs)
 
     sol = vs
@@ -260,8 +326,65 @@ def runusolve(fprime=None):
     rsol = obj(sol)
 
     errsol = relNormMax(sol, np.array([R0, theta0, phi0]))
-    print(f'CG solution {sol}, expected {[R0, theta0, phi0]}')
-    print(f'CG solution error {errsol}, final objective {rsol}')
+    print(f'usolve solution {sol}, expected {[R0, theta0, phi0]}')
+    print(f'usolve solution error {errsol}, final objective {rsol}')
+
+
+def runusolve_fd():
+
+    def gobj(fobj, x, y, g, udata):
+        (dr, r) = pyfad.DiffFD(fobj, x)
+        y[:] = r
+        for i in range(x.size):
+            g[:,i] = dr[i]
+        print(f'gobj(x) = {np.linalg.norm(y)}')
+
+    def gvobj(fobj, x, y, dx, g, udata):
+        #print(f'dx={dx.shape}, g={g.shape}')
+        if True:
+            (drF, rF) = pyfad.DiffFD(fobj, x)
+            Jac = np.zeros((rF.size, x.size))
+            for i in range(x.size):
+                Jac[:,i] = drF[i]
+            Jv = Jac @ dx
+            if False:
+                print('check Jac', Jac)
+                print('check dx', dx)
+                print('check', Jv)
+                (drnp, rnp) = pyfad.DiffFDNP(fobj, x, seed=[dx ])
+                print('check fd2', drnp)
+                (drnp2, rnp2) = pyfad.DiffFDNP(lambda t: fobj(x + t*dx.flat), np.array([0.0]))
+                print('check fd2', drnp2)
+                (dr, r) = pyfad.DiffFD(fobj, x, seed=[dx ])
+        y[:] = rF
+        g[:] = Jv
+        print(f'gvobj(x) = {np.linalg.norm(y)}')
+
+    _runusolve(gobj, gvobj)
+
+
+def runusolve_ad():
+    import uopt.uopt
+    import scipy as sc
+
+    objComps, obj, handle = cylfit_obj()
+
+    def gobj(fobj, x, y, g, udata):
+        (dr, r) = pyfad.DiffFor(fobj, x)
+        y[:] = r
+        for i in range(x.size):
+            g[:,i] = dr[i]
+        print(f'gobj(x) = {np.linalg.norm(y)}')
+
+    def gvobj(fobj, x, y, dx, g, udata):
+        (dr, r) = pyfad.DiffFor(fobj, x, seed=[dx])
+        y[:] = r
+        N, Ndd = dx.shape
+        for i in range(Ndd):
+            g[:,i] = dr[i]
+        print(f'gvobj(x) = {np.linalg.norm(y)}')
+
+    _runusolve(gobj, gvobj)
 
 
 if __name__ == "__main__":
