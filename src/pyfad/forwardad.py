ac0e914ae043a220ee2a12d1d6cb25b57a69bcb1
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-18 18:36:59 +0200

    Further tuning... now we have three different modes.
    Usally kw is not needed, so rules now have to announce they want it if they need it

--- src/pyfad/forwardad.py
+++ src/pyfad/forwardad.py
@@ -11,23 +11,30 @@ me = sys.modules[__name__]
 
 def mkRule(f, rule):
     def runRule(*args, **kw):
-        d_kw, kw = unjnd(kw)
         res = f(*args[1::2], **kw)
-        dres = rule(res, *args, **d_kw)
+        dres = rule(res, *args, **kw)
         return dres, res
-    runRule.builtin = True
     return runRule
+
 
 def mkRule2(f, rule):
     def runRule2(*args, **kw):
-        return rule(*args, **kw)
-    runRule2.builtin = True
+        d_kw, kw = unjnd(kw)
+        res = f(*args[1::2], **kw)
+        dres = rule(res, d_kw, *args, **kw)
+        return dres, res
     return runRule2
+
+
+def mkRule3(f, rule):
+    return rule
+
 
 def initType(function, *args, **kw):
     do, o = function(*args[1::2], **kw), function(*args[1::2], **kw)
     do = dzeros(do)
     return do, o
+
 
 def decorator(**opts):
 
@@ -39,11 +46,17 @@ def decorator(**opts):
         if rule is not None:
             return mkRule(f, rule)
 
-        id = 'E_' + rid(f)
+        id = 'Dkw_' + rid(f)
         rule = getattr(me, id, None)
 
         if rule is not None:
             return mkRule2(f, rule)
+
+        id = 'E_' + rid(f)
+        rule = getattr(me, id, None)
+
+        if rule is not None:
+            return mkRule3(f, rule)
 
         # try source diff
         return done(key)
@@ -82,7 +95,7 @@ def E_builtins_object___init__(*args, **kw):
     #print('E_object___init___builtins', *args)
     return args[0], args[1]
 
-def D_builtins_dict(r, *args, **d_kw):
+def Dkw_builtins_dict(r, d_kw, *args, **kw):
     return dict(**d_kw)
 
 def D_builtins_dict_items(r, dx, x):
@@ -182,15 +195,14 @@ def D_numpy_matmul(r, dx, x, dy, y):
 def E_builtins_ndarray_copy(*args, **kw):
     return args[0].copy(),  args[1].copy()
 
-def E_builtins_ndarray_reshape(*args, **kw):
-    return args[0].reshape(*args[3::2]),  args[1].reshape(*args[3::2])
+def D_builtins_ndarray_reshape(r, *args, **kw):
+    return args[0].reshape(*args[3::2])
 
 def D_numpy_hstack(r, dx, x):
     return np.hstack(dx)
 
-def E_numpy_sum(dx, x, **kw):
-    d_kw, kw = unjnd(kw)
-    return np.sum(dx, **kw), np.sum(x, **kw)
+def Dkw_numpy_sum(r, d_kw, dx, x, **kw):
+    return np.sum(dx, **kw)
 
 def D_numpy_diag(r, dx, x):
     return np.diag(dx)
@@ -198,8 +210,21 @@ def D_numpy_diag(r, dx, x):
 def D_numpy_array(r, dx, x):
     return np.array(dx)
 
-def D_numpy_linalg_norm(r, dx, x):
-    return np.sum(x * dx) / r
+def D_numpy_linalg_norm(r, dx, x, d_ord=0, ord=None):
+    if ord is None:
+        return np.sum(x * dx) / r
+    else:
+        if ord == 2:
+            if x.ndim > 1:
+                U, S, Vh = np.linalg.svd(x)
+                maxv = S[0]
+                dn = U[:,0] @ dx @ Vh[:,0]
+                return dn
+            else:
+                raise ValueError()
+                return np.sum(x * dx) / r
+        else:
+            return np.sum(x * dx) / r
 
 def D_numpy_real(r, dx, x):
     return np.real(dx)
