66aecca4397bead0fe5380be83fa684108c61a63
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-18 18:45:06 +0200

    Demos

---
+++ tests/demos/demo_babylonian.py
@@ -0,0 +1,24 @@
+import pyfad
+
+def gbabylonian(x, y=1):
+    if abs(y**2 - x) < 1e-7:
+        return y
+    else:
+        r = gbabylonian(x, (y + x / y) / 2)
+        return r
+
+def fbabylonian(x):
+    r = gbabylonian(abs(x))
+    return r
+
+# define input value
+x = 16
+
+# compute function result
+r = fbabylonian(x)
+print(f'f{x}) = {r}')
+
+#compute derivative
+d_r, r = pyfad.DiffFor(fbabylonian, x)
+print(f'f{x}) = {r}')
+print(f'f\'{x}) = {d_r}, correct result is {0.5/r}, error {d_r[0] - 0.5/r}')
---
+++ tests/demos/demo_babyloniannp.py
@@ -0,0 +1,27 @@
+import pyfad
+import numpy as np
+
+#vectorized gbabylonian
+def gbabylonian(x, y=1):
+    if np.all(np.abs(y**2 - x) < 1e-7):
+        return y
+    else:
+        r = gbabylonian(x, (y + x / y) / 2)
+        return r
+
+def fbabylonian(x):
+    r = gbabylonian(x)
+    return r
+
+# define input value
+x = np.arange(1, 17)
+
+# compute function result
+r = fbabylonian(x)
+print(f'f{x}) = {r}')
+
+#compute derivative
+d_r, r = pyfad.DiffFor(fbabylonian, x, seed = [np.ones(16)])
+print(f'f{x}) = {r}')
+print(f'f\'{x}) = {d_r}, correct result is {0.5/r},\
+ error {np.linalg.norm(d_r[0] - 0.5/r)}')
