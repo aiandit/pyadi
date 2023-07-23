a57da47131fd0ef1af5969341db101684f48ff02
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-07-23 22:07:23 +0200

    Starred expression may be any, List literal, for examples

--- src/pyadi/pyadi.py
+++ src/pyadi/pyadi.py
@@ -438,7 +438,7 @@ Calls methods self._DXYZ for individual node XYZ handling
         return getattr(self.getRoot(t), 'id', '') in self.localvars
 
     def _DStarred(self, node):
-        node.value = Call('zip', [self.ddispatch(node.value.clone()), node.value])
+        node.value = Call('zip', [Starred(self.diffUnlessIsTupleDiff(node.value))])
         return node
 
     def _DSubscript(self, node):
