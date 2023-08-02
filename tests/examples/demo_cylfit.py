eb693e4ecd5519b33ac2f1bc2c1999266d6d5f47
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-08-02 23:32:44 +0200

    Optim. demo

--- tests/examples/demo_cylfit.py
+++ tests/examples/demo_cylfit.py
@@ -1,9 +1,10 @@
 """Demo demo_cylfit
 ================
 
-This demo module shows how to pass differentiate a real lite function
+This demo module shows how to pass differentiate a real life function
 using :py:mod:`numpy` and to perform an optimization using AI & IT's
-solver usolve with the AD derivatives.
+solver `UOpt usolve <https://ai-and-it.de/uopt.html>`_ with the AD
+derivatives.
 
 """
 
@@ -17,7 +18,49 @@ from .cylfit import cylfit_obj, mkCylData
 #from .cylfit2 import cylfit_obj, mkCylData
 
 def run():
-    """
+    """Set up the problem and the derivative functions for the solver
+    and start it.
+
+    Input values for the three parameters and the probkem size are
+    set::
+
+       R0 = 1.1
+       theta0 = 0.1
+       phi0 = np.pi/2
+
+       N = int(np.sqrt(190))**2
+
+    Sample points are created with thse values with
+    :py:func:`mkCylData`.
+
+    :py:func:`cylfit_obj` is a constructor function that returns the
+    objective function and a handle function.
+
+    The handle function is used to set the point in a variable in the
+    scobe of the objective function.
+
+    The solve needs the starting vector, the solution and the optimal
+    result as :py:mod:`numpy` arrays::
+
+      v0 = np.array([1, 0.01, 0.01])
+
+      r0 = objComps(v0)
+
+      vs = v0.copy()
+      rs = r0.copy()
+
+    Then the optimization can bestarted with
+
+      res = uopt.usolve(v0, vs, rs,
+                        fobj, gobj, gvobj,
+                        s = status)
+      print('usolve result', res, vs)
+
+
+    The ``fobj``, ``gobj``, and ``gvobj`` are function handles that
+    compute the function, the full Jacobian and Jacobian-vector
+    products,
+
     """
 
     objComps, obj, handle = cylfit_obj()
@@ -43,9 +86,6 @@ def run():
     theta0 = 0.1
     phi0 = np.pi/2
 
-    # v0 = np.array([1, 0, 0])
-    v0 = np.array([1, 0.01, 0.01])
-
     N = int(np.sqrt(190))**2
 
     demopts = mkCylData(N, R0, theta0, phi0)
@@ -53,6 +93,9 @@ def run():
     #demopts += (np.random.rand(N, 3) - 0.5) * 1e-6
 
     handle()['points'] = demopts
+
+    # v0 = np.array([1, 0, 0])
+    v0 = np.array([1, 0.01, 0.01])
 
     r0 = objComps(v0)
 
